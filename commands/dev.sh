84790b26249671fb217a65f6a3ac7d04657485fa
spacecodeur <spacecodeur@gmail.com>
2025-02-08 13:27:12 +0100

    re-organize commands dir inner new struct

--- commands/dev.sh
+++ commands/dev.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-./commands.sh docker-clean-with-cache
+./commands.sh docker/clean-with-cache
 
-./commands.sh app-container-build &
-./commands.sh db-container-build &
+./commands.sh app/container/build &
+./commands.sh db/container/build &
 wait
 
-./commands.sh fc-app-migrate
-./commands.sh fc-app-wasm-build
+./commands.sh fc/app/migrate/run
+./commands.sh fc/app/wasm-build
 
 echo
 echo "All set!"
-echo "Next step : ./commands.sh fc-app-server-start"
+echo "Next step : ./commands.sh fc/app/server-start"
 echo "Happy coding! :D"
