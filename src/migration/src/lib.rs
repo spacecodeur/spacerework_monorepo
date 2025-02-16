85f6a76d2c9cbd60b50b0abb4bff144af2061827
spacecodeur <spacecodeur@gmail.com>
2025-02-16 19:22:10 +0100

    chore(): remove some dead code

--- src/migration/src/lib.rs
+++ src/migration/src/lib.rs
@@ -10,7 +10,6 @@ impl MigratorTrait for Migrator {
             Box::new(m20250213_231309_create_table_lesson::Migration),
             Box::new(m20250215_125449_insert_main_trainers::Migration),
             Box::new(m20250215_141600_create_table_pathsegment::Migration),
-            // Box::new(m20250215_141606_insert_some_data_for_pathlesson_tests::Migration),
         ]
     }
 }
@@ -18,4 +17,3 @@ mod m20250213_230915_create_table_user;
 mod m20250213_231309_create_table_lesson;
 mod m20250215_125449_insert_main_trainers;
 mod m20250215_141600_create_table_pathsegment;
-// mod m20250215_141606_insert_some_data_for_pathlesson_tests;
