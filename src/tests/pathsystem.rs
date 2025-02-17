be2f08e2ff71c35bcad11b14143a26cf8f57ab18
spacecodeur <spacecodeur@gmail.com>
2025-02-17 03:12:11 +0100

    fixup! Merge branch 'main' into pathlesson

--- src/tests/pathsystem.rs
+++ src/tests/pathsystem.rs
@@ -1,33 +1,32 @@
-use crate::domain::pathsystem::is_dir;
+use crate::domain::pathsystem::is_dir_semantic_check;
 
 #[test]
 fn check_if_dir() {
-    let result = is_dir("toto/titi/tata");
+    let result = is_dir_semantic_check("toto/titi/tata");
     assert_eq!(result, Ok(true));
-    let result = is_dir("cours");
+    let result = is_dir_semantic_check("cours");
     assert_eq!(result, Ok(true));
-    let result = is_dir("");
+    let result = is_dir_semantic_check("");
     assert_eq!(result, Ok(true));
 }
 
 #[test]
 fn check_if_file() {
-    let result = is_dir("toto/titi/tata/cours.md");
-    assert_eq!(result,Ok(false));
+    let result = is_dir_semantic_check("toto/titi/tata/cours.md");
+    assert_eq!(result, Ok(false));
 
-    let result = is_dir("lesson.md");
-    assert_eq!(result,Ok(false));
-
+    let result = is_dir_semantic_check("lesson.md");
+    assert_eq!(result, Ok(false));
 }
 
 #[test]
 fn check_if_malformed() {
-    let result = is_dir("toto/titi/cours.md/tata");
+    let result = is_dir_semantic_check("toto/titi/cours.md/tata");
     assert_eq!(result, Err("malformed path"));
-    let result = is_dir("toto/titi/cours.coucou/tata");
+    let result = is_dir_semantic_check("toto/titi/cours.coucou/tata");
     assert_eq!(result, Err("malformed piece of path"));
-    let result = is_dir("toto/titi/cours.coucou");
+    let result = is_dir_semantic_check("toto/titi/cours.coucou");
     assert_eq!(result, Err("malformed piece of path"));
-    let result = is_dir("toto/titi/cou??rs/tata");
+    let result = is_dir_semantic_check("toto/titi/cou??rs/tata");
     assert_eq!(result, Err("malformed path"));
 }
