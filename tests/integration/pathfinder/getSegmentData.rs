7d15cfdbeb4a0c38c5d1a52fdd5043d4141e0b21
Marc-Antoine Berger <mab.dev69@gmail.com>
2025-02-25 15:42:04 +0000

    up

---
+++ tests/integration/pathfinder/getSegmentData.rs
@@ -0,0 +1,16 @@
+use app::domain::pathsystem::is_dir_semantic_check;
+
+#[test]
+fn isInvalidPersistentSegment() {
+    // check if asked path is not stored in db
+}
+
+#[test]
+fn getContentLessonFromValidSegment() {}
+
+#[test]
+fn getChildsOfValidSegment() {}
+
+#[test]
+fn getEmptyArrayOfEmptyRootTrainer() {}
+
