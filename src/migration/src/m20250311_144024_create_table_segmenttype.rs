88202d1b296167ba216a2a9e7e6d199847f649f4
spacecodeur <spacecodeur@gmail.com>
2025-03-17 17:34:22 +0100

    chore(database): replace the type of the field name to an enum

--- src/migration/src/m20250311_144024_create_table_segmenttype.rs
+++ src/migration/src/m20250311_144024_create_table_segmenttype.rs
@@ -1,4 +1,7 @@
-use sea_orm_migration::{prelude::*, schema::*};
+use sea_orm_migration::{
+    prelude::{extension::postgres::Type, *},
+    schema::*,
+};
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
@@ -7,17 +10,33 @@ pub struct Migration;
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         manager
+            .create_type(
+                Type::create()
+                    .as_enum(SegmentTypeName::Enum)
+                    .values([SegmentTypeName::Directory, SegmentTypeName::Lesson])
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
             .create_table(
                 Table::create()
                     .table(SegmentType::Table)
                     .col(pk_auto(SegmentType::Id))
-                    .col(string(SegmentType::Name))
+                    .col(ColumnDef::new(SegmentType::Name).enumeration(
+                        SegmentTypeName::Enum,
+                        [SegmentTypeName::Directory, SegmentTypeName::Lesson],
+                    ))
                     .to_owned(),
             )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_type(Type::drop().name(SegmentTypeName::Enum).to_owned())
+            .await?;
+
         manager
             .drop_table(Table::drop().table(SegmentType::Table).to_owned())
             .await
@@ -29,4 +48,14 @@ pub enum SegmentType {
     Table,
     Id,
     Name,
+}
+
+#[derive(DeriveIden)]
+enum SegmentTypeName {
+    #[sea_orm(iden = "Name")]
+    Enum,
+    #[sea_orm(iden = "directory")]
+    Directory,
+    #[sea_orm(iden = "lesson")]
+    Lesson,
 }
